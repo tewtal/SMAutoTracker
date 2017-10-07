dd5131acf7aab1d0f9429c9230d38fde18005d64
Thomas Backmark <total@viskos.org>
2017-10-07 20:19:25 +0200

    Add project files.

---
+++ SMAutoTracker/MainWindow.xaml.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.Configuration;
+using usb2snes.core;
+
+namespace SMAutoTracker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Tracker _tracker;
+        private DispatcherTimer _pollTimer;
+        private string _comPort;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _tracker = new Tracker();
+            this.DataContext = _tracker;
+
+            var devices = core.GetDeviceList();
+            if (devices.Count > 0)
+            {
+                _comPort = devices[0].Name;
+            }
+            else
+            {
+                _comPort = ConfigurationManager.AppSettings["COMPort"];
+                if (ShowInputDialog(ref _comPort) != System.Windows.Forms.DialogResult.OK)
+                {
+                    Application.Current.Shutdown();
+                }
+            }
+
+            try
+            {
+                core.Connect(_comPort);
+                if (core.Connected())
+                {
+                    ConfigurationManager.AppSettings["COMPort"] = _comPort;
+                }
+                else
+                {
+                    MessageBox.Show("Could not connect to usb2snes, check COM-port setting.");
+                    Application.Current.Shutdown();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Could not connect to usb2snes, check COM-port setting.");
+                Application.Current.Shutdown();
+            }
+
+            _pollTimer = new DispatcherTimer();
+            _pollTimer.Tick += _pollTimer_Tick;
+            _pollTimer.Interval = new TimeSpan(0, 0, 0, 0, 100);
+            _pollTimer.Start();
+
+        }
+
+        private void _pollTimer_Tick(object sender, EventArgs e)
+        {
+            if(core.Connected())
+            {
+                var data = new byte[512];
+                core.SendCommand(core.usbint_server_opcode_e.USBINT_SERVER_OPCODE_VGET, core.usbint_server_space_e.USBINT_SERVER_SPACE_SNES, core.usbint_server_flags_e.USBINT_SERVER_FLAGS_NONE, Tuple.Create(0xF509A0, 64), Tuple.Create(0xF5D800, 64));
+                core.GetData(data, 0, 512);
+
+                _tracker.UpdateItems(data[0x04] + (data[0x05] << 8));
+                _tracker.UpdateBeams(data[0x08] + (data[0x09] << 8));
+
+                _tracker.Missiles = (data[0x28] + (data[0x29] << 8));
+                _tracker.Supers = (data[0x2C] + (data[0x2D] << 8));
+                _tracker.PBs = (data[0x30] + (data[0x31] << 8));
+                _tracker.ETanks = (data[0x24] + (data[0x25] << 8)) / 100;
+                _tracker.Reserves = (data[0x34] + (data[0x35] << 8)) / 100;
+
+                _tracker.UpdateBosses(data[0x69], data[0x6A], data[0x6B], data[0x6C]);
+            }
+        }
+
+        private static System.Windows.Forms.DialogResult ShowInputDialog(ref string input)
+        {
+            System.Drawing.Size size = new System.Drawing.Size(300, 70);
+            System.Windows.Forms.Form inputBox = new System.Windows.Forms.Form();
+
+            inputBox.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            inputBox.ClientSize = size;
+            inputBox.Text = "Enter USB2Snes COM-port";
+
+            System.Windows.Forms.TextBox textBox = new System.Windows.Forms.TextBox();
+            textBox.Size = new System.Drawing.Size(size.Width - 10, 23);
+            textBox.Location = new System.Drawing.Point(5, 5);
+            textBox.Text = input;
+            inputBox.Controls.Add(textBox);
+
+            System.Windows.Forms.Button okButton = new System.Windows.Forms.Button();
+            okButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            okButton.Name = "okButton";
+            okButton.Size = new System.Drawing.Size(75, 23);
+            okButton.Text = "&OK";
+            okButton.Location = new System.Drawing.Point(size.Width - 80 - 80, 39);
+            inputBox.Controls.Add(okButton);
+
+            System.Windows.Forms.Button cancelButton = new System.Windows.Forms.Button();
+            cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            cancelButton.Name = "cancelButton";
+            cancelButton.Size = new System.Drawing.Size(75, 23);
+            cancelButton.Text = "&Cancel";
+            cancelButton.Location = new System.Drawing.Point(size.Width - 80, 39);
+            inputBox.Controls.Add(cancelButton);
+
+            inputBox.AcceptButton = okButton;
+            inputBox.CancelButton = cancelButton;
+
+            System.Windows.Forms.DialogResult result = inputBox.ShowDialog();
+            input = textBox.Text;
+            return result;
+        }
+    }
+}
---
+++ SMAutoTracker/Tracker.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace SMAutoTracker
+{
+    public class Tracker : INotifyPropertyChanged
+    {
+        private int _missiles;
+        public int Missiles { get { return _missiles; } set { _missiles = value; OnPropertyChanged(); OnPropertyChanged("HaveMissiles"); } }
+        public bool HaveMissiles {  get { return _missiles > 0;  } }
+
+        private int _supers;
+        public int Supers { get { return _supers; } set { _supers = value; OnPropertyChanged(); OnPropertyChanged("HaveSupers"); } }
+        public bool HaveSupers { get { return _supers > 0; } }
+
+        private int _pbs;
+        public int PBs { get { return _pbs; } set { _pbs = value; OnPropertyChanged(); OnPropertyChanged("HavePBs"); } }
+        public bool HavePBs { get { return _pbs > 0; } }
+
+        private int _etanks;
+        public int ETanks { get { return _etanks; } set { _etanks = value; OnPropertyChanged(); OnPropertyChanged("HaveETanks"); } }
+        public bool HaveETanks { get { return _etanks > 0; } }
+
+        private int _reserves;
+        public int Reserves { get { return _reserves; } set { _reserves = value; OnPropertyChanged(); OnPropertyChanged("HaveReserves"); } }
+        public bool HaveReserves { get { return _reserves > 0; } }
+
+        private bool _morph;
+        public bool Morph { get { return _morph; } set { _morph = value; OnPropertyChanged(); } }
+
+        private bool _bombs;
+        public bool Bombs { get { return _bombs; } set { _bombs = value; OnPropertyChanged(); } }
+
+        private bool _springBall;
+        public bool SpringBall { get { return _springBall; } set { _springBall = value; OnPropertyChanged(); } }
+
+        private bool _hiJump;
+        public bool HiJump { get { return _hiJump; } set { _hiJump = value; OnPropertyChanged(); } }
+
+        private bool _spaceJump;
+        public bool SpaceJump { get { return _spaceJump; } set { _spaceJump = value; OnPropertyChanged(); } }
+
+        private bool _varia;
+        public bool Varia { get { return _varia; } set { _varia = value; OnPropertyChanged(); } }
+
+        private bool _gravity;
+        public bool Gravity { get { return _gravity; } set { _gravity = value; OnPropertyChanged(); } }
+
+        private bool _xray;
+        public bool XRay { get { return _xray; } set { _xray = value; OnPropertyChanged(); } }
+
+        private bool _grapple;
+        public bool Grapple { get { return _grapple; } set { _grapple = value; OnPropertyChanged(); } }
+
+        private bool _speed;
+        public bool Speed { get { return _speed; } set { _speed = value; OnPropertyChanged(); } }
+
+        private bool _screw;
+        public bool ScrewAttack { get { return _screw; } set { _screw = value; OnPropertyChanged(); } }
+
+        private bool _charge;
+        public bool Charge { get { return _charge; } set { _charge = value; OnPropertyChanged(); } }
+
+        private bool _ice;
+        public bool Ice { get { return _ice; } set { _ice = value; OnPropertyChanged(); } }
+
+        private bool _wave;
+        public bool Wave { get { return _wave; } set { _wave = value; OnPropertyChanged(); } }
+
+        private bool _spazer;
+        public bool Spazer { get { return _spazer; } set { _spazer = value; OnPropertyChanged(); } }
+
+        private bool _plasma;
+        public bool Plasma { get { return _plasma; } set { _plasma = value; OnPropertyChanged(); } }
+
+        private bool _kraid;
+        public bool Kraid { get { return _kraid; } set { _kraid = value; OnPropertyChanged(); } }
+
+        private bool _phantoon;
+        public bool Phantoon { get { return _phantoon; } set { _phantoon = value; OnPropertyChanged(); } }
+
+        private bool _draygon;
+        public bool Draygon { get { return _draygon; } set { _draygon = value; OnPropertyChanged(); } }
+
+        private bool _ridley;
+        public bool Ridley { get { return _ridley; } set { _ridley = value; OnPropertyChanged(); } }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([System.Runtime.CompilerServices.CallerMemberName] string propertyName = "")
+        {
+            if (this.PropertyChanged != null)
+            {
+                var e = new PropertyChangedEventArgs(propertyName);
+                this.PropertyChanged(this, e);
+            }
+        }
+
+        public Tracker()
+        {
+        }
+
+        public void UpdateItems(int itemBits)
+        {
+            Varia = ((itemBits & 0x01) != 0);
+            SpringBall = ((itemBits & 0x02) != 0);
+            Morph = ((itemBits & 0x04) != 0);
+            ScrewAttack = ((itemBits & 0x08) != 0);
+            Gravity = ((itemBits & 0x20) != 0);
+
+            HiJump = ((itemBits & 0x0100) != 0);
+            SpaceJump = ((itemBits & 0x0200) != 0);
+            Bombs = ((itemBits & 0x1000) != 0);
+            Speed = ((itemBits & 0x2000) != 0);
+            Grapple = ((itemBits & 0x4000) != 0);
+            XRay = ((itemBits & 0x8000) != 0);
+        }
+
+        public void UpdateBeams(int beamBits)
+        {
+            Wave = ((beamBits & 0x0001) != 0);
+            Ice = ((beamBits & 0x0002) != 0);
+            Spazer = ((beamBits & 0x0004) != 0);
+            Plasma = ((beamBits & 0x0008) != 0);
+
+            Charge = ((beamBits & 0x1000) != 0);
+        }
+
+        public void UpdateBosses(int brinstar, int norfair, int ws, int maridia)
+        {
+            Kraid = ((brinstar & 0x01) != 0);
+            Ridley = ((norfair & 0x01) != 0);
+            Phantoon = ((ws & 0x01) != 0);
+            Draygon = ((maridia & 0x01) != 0);
+        }
+    }
+
+
+    [ValueConversion(typeof(bool), typeof(double))]
+    public class BooleanToOpacityConverter : IValueConverter
+    {
+        #region Fields
+
+        private bool _not = false;
+
+        #endregion
+
+        public bool Not { get { return _not; } set { _not = value; } }
+
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (Not)
+            {
+                if (!(bool)value)
+                {
+                    return 1;
+                }
+                else
+                {
+                    double val = 0.65;
+
+                    if (parameter != null)
+                    {
+                        double.TryParse(parameter.ToString(), System.Globalization.NumberStyles.Any, CultureInfo.InvariantCulture, out val);
+                    }
+
+                    return val;
+                }
+            }
+            else
+            {
+                if ((bool)value)
+                {
+                    return 1;
+                }
+                else
+                {
+                    double val = 0.65;
+
+                    if (parameter != null)
+                    {
+                        double.TryParse(parameter.ToString(), System.Globalization.NumberStyles.Any, CultureInfo.InvariantCulture, out val);
+                    }
+
+                    return val;
+                }
+            }
+
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
